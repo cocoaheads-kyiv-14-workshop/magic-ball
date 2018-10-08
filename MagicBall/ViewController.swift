ac8daf448e6bc3a1a19d9bb7f682c1c4c57b1367
Alexander Voronov <voronovaleksandr91@gmail.com>
2018-10-09 01:03:51 +0300

    created main project

---
+++ MagicBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicBall
+//
+//  Created by Alexander Voronov on 10/9/18.
+//  Copyright © 2018 Oleksandr Voronov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
