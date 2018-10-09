21c79363fb82aeca1138537e94d4bfdf193f7a93
Alexander Voronov <voronovaleksandr91@gmail.com>
2018-10-10 01:20:45 +0300

    created Tutorial local pod

---
+++ Pods_Local/Tutorial/Example/Tutorial/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Tutorial
+//
+//  Created by a-voronov on 10/10/2018.
+//  Copyright (c) 2018 a-voronov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
