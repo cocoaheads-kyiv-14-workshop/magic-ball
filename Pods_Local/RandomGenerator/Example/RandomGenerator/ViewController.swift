cd40df01f15f716b74360b77873b821c1e1c2605
Alexander Voronov <voronovaleksandr91@gmail.com>
2018-10-09 03:18:04 +0300

    created RandomGenerator local pod

---
+++ Pods_Local/RandomGenerator/Example/RandomGenerator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RandomGenerator
+//
+//  Created by a-voronov on 10/09/2018.
+//  Copyright (c) 2018 a-voronov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
