d480a108e00c07a888a20ae492d4a4f9f51cb777
Alexander Voronov <voronovaleksandr91@gmail.com>
2018-10-09 03:22:04 +0300

    implemented module functionality

--- Pods_Local/RandomGenerator/Example/RandomGenerator/ViewController.swift
+++ Pods_Local/RandomGenerator/Example/RandomGenerator/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import RandomGenerator
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        RandomGenerator().exec()
     }
 
     override func didReceiveMemoryWarning() {
